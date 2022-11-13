a4283e30ca2a7c407b1d26c0134494fc96a57785
Cory Buecker <email@corybuecker.com>
2022-11-13 09:11:28 -0600

    Add CD workflow (#28)
    New CD workflow

--- lib/simple_budget/utilities/fake_date.ex
+++ lib/simple_budget/utilities/fake_date.ex
@@ -1,17 +1,15 @@
 defmodule SimpleBudget.Utilities.FakeDate do
   use Agent
 
+  @spec start_link :: {:ok, pid}
   def start_link() do
-    Agent.start_link(fn -> "2020-01-15" |> Date.from_iso8601!() end,
-      name: SimpleBudget.Utilities.FakeDate
-    )
+    {:ok, _pid} =
+      Agent.start_link(fn -> "2020-01-15" |> Date.from_iso8601!() end,
+        name: SimpleBudget.Utilities.FakeDate
+      )
   end
 
   def today() do
     Agent.get(SimpleBudget.Utilities.FakeDate, fn val -> val end)
-  end
-
-  def set(value) do
-    Agent.update(SimpleBudget.Utilities.FakeDate, fn _val -> value |> Date.from_iso8601!() end)
   end
 end
