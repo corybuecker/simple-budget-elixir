9cdede837fe6ea526338ac6f0206be1a382f2710
Cory Buecker <email@corybuecker.com>
2023-10-06 07:37:17 -0500

    update account edit form

--- lib/simple_budget_web/components/custom_components.ex
+++ lib/simple_budget_web/components/custom_components.ex
@@ -1,14 +1,18 @@
 defmodule SimpleBudgetWeb.CustomComponents do
   use Phoenix.Component
 
-  attr :name, :string, required: true
+  attr :field, Phoenix.HTML.FormField
   attr :label, :string, required: true
+  attr :rest, :global
 
-  def text_input(assigns) do
+  def text_input(%{field: %Phoenix.HTML.FormField{}} = assigns) do
     ~H"""
     <div class="flex flex-col">
-      <label for={assigns.name}><%= assigns.label %></label>
-      <input type="text" id={assigns.name} name={assigns.name} />
+      <label for={@field.name}><%= @label %></label>
+      <input type="text" id={@field.name} name={@field.name} value={@field.value} {@rest} />
+      <%= for {error, _} <- @field.errors do %>
+        <div class="text-sm"><%= error %></div>
+      <% end %>
     </div>
     """
   end
