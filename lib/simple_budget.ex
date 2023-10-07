d18f5e88a10f3c1e31a2780be52ca598407712b8
Cory Buecker <email@corybuecker.com>
2023-10-07 07:04:54 -0500

    Update simple_budget.ex (#104)

--- lib/simple_budget.ex
+++ lib/simple_budget.ex
@@ -1,7 +1,6 @@
 defmodule SimpleBudget do
   @moduledoc """
-  SimpleBudget keeps the contexts that define your domain
-  and business logic.
+  SimpleBudget keeps the contexts that define your domain and business logic.
 
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
