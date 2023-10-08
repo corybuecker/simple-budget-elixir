6b3712850f453152bef07591f8cc422c518bf3b8
Cory Buecker <email@corybuecker.com>
2023-10-08 07:57:06 -0500

    replace changeset with to_form

--- lib/simple_budget_web/live/account_live/edit.ex
+++ lib/simple_budget_web/live/account_live/edit.ex
@@ -3,25 +3,27 @@ defmodule SimpleBudgetWeb.AccountLive.Edit do
   require Logger
 
   def mount(params, session, socket) do
+    user = SimpleBudget.Users.get_by_identity(session["identity"])
     account = SimpleBudget.Accounts.get(session, params)
-    changeset = SimpleBudget.Account.changeset(account)
 
     {:ok,
      socket
-     |> assign(%{page_title: "Accounts"})
-     |> assign(%{changeset: changeset, account: account, identity: session["identity"]})}
+     |> assign(page_title: "Accounts")
+     |> assign(preferences: user.preferences)
+     |> assign(account: account)
+     |> assign(identity: session["identity"])
+     |> assign(form: to_form(SimpleBudget.Account.changeset(account)))}
   end
 
   def handle_event("validate", %{"account" => params}, socket) do
-    changeset =
-      SimpleBudget.Account.changeset(socket.assigns.account, params)
-      |> Map.put(:action, :validate)
-
-    Logger.debug(changeset)
-
     {:noreply,
      socket
-     |> assign(:changeset, changeset)}
+     |> assign(
+       :form,
+       SimpleBudget.Account.changeset(socket.assigns.account, params)
+       |> Map.put(:action, :validate)
+       |> to_form()
+     )}
   end
 
   def handle_event("save", %{"account" => params}, socket) do
@@ -31,10 +33,37 @@ defmodule SimpleBudgetWeb.AccountLive.Edit do
 
     case result do
       {:error, changeset} ->
-        {:noreply, socket |> assign(:changeset, changeset)}
+        {:noreply, socket |> assign(:form, changeset |> to_form())}
 
       {:ok, _} ->
         {:noreply, socket |> push_navigate(to: "/accounts")}
     end
+  end
+
+  def render(assigns) do
+    ~H"""
+    <%= if @preferences.show_changeset do %>
+      <%= @form |> inspect() %>
+    <% end %>
+
+    <.form for={@form} phx-change="validate" phx-submit="save">
+      <div>
+        <.text_input label="Name" field={@form[:name]} />
+        <.text_input
+          field={@form[:balance]}
+          inputmode="decimal"
+          label="Balance"
+          min="0"
+          step="0.01"
+          type="number"
+        />
+        <.input type="checkbox" field={@form[:debt]} label="Debt"></.input>
+        <div>
+          <.custom_button type="submit">Save</.custom_button>
+          <.link navigate="/accounts">Cancel</.link>
+        </div>
+      </div>
+    </.form>
+    """
   end
 end
