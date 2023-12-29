17b64098d50ea53e610a69fcd248ec9c44f19d20
Cory Buecker <email@corybuecker.com>
2023-12-29 05:25:33 -0600

    remove PWA and update deps

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+%SimpleBudget.User{
+  email: System.get_env("ADMIN_EMAIL"),
+  identity: Ecto.UUID.generate(),
+  preferences: %{}
+}
+|> SimpleBudget.Repo.insert!()
